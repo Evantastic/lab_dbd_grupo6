e332a06ccb8e7dcdfd67f900dcaa2d33bf0b05d3
Alan Barahona <alan.barahona@usach.cl>
2018-11-26 14:30:26 -0300

    Agregado vehiculo a la base de datos

---
+++ database/migrations/2018_11_26_155240_create_vehiculos_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateVehiculosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('vehiculos', function (Blueprint $table) {
+            $table->increments('id');
+            $table->intenger('automotora_id');
+            $table->string('marca',31);
+            $table->string('modelo',31);
+            $table->string('tipo',31);
+            $table->string('patente',15);
+            //Revisar$table->integer('precio');
+            $table->smallInteger('capacidad');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('vehiculos');
+    }
+}
