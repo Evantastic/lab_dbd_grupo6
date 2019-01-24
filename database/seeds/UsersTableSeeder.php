9ad7b49878387278c2c0676a1e79a2c9eec1b3be
Alan Alejandro Barahona Ruiz <alan.barahona@usach.cl>
2019-01-23 22:55:54 -0300

    Agregado login basico

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -11,6 +11,15 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
+        DB::table('users')->insert([
+            'name' => 'admin',
+            'apellido' => 'admin',
+            'nacionalidad' => 'admin',
+            'edad' => 21,
+            'tipoUsuario' => 1,
+            'email' => 'admin@admin.cl',
+            'password' => bcrypt('admin'),
+        ]);
        factory(User::class,100)->create(); //
     }
 }
