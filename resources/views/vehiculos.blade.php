4ffb6d3312e31ec7559f24c3836bb6ef2d0c832e
Unknown <claudio.urrutia.b@usach.cl>
2019-03-05 17:11:42 -0300

    Busquedas

--- resources/views/vehiculos.blade.php
+++ resources/views/vehiculos.blade.php
@@ -2,6 +2,34 @@
 @section('content')
 <div class="container">
       <!-- Page Heading -->
+
+        <form action="/vehiculo/buscar" method="get">
+            <div class="form-group row">
+                <label for="nombreInput" class="col-2 col-form-label">Ciudad</label>
+                <div class="col-10">
+                    <input class="form-control" type="text" value="" id="nombreInput" name="nombre_ciudad">
+                </div>
+            </div>
+            <div class="form-group row">
+                <label for="apellidoInput" class="col-2 col-form-label">Fecha Inicio</label>
+                <div class="col-10">
+                    <input class="form-control" type="text" value="" id="apellidoInput" name="fecha_inicio">
+                    <small id="medioHelp" class="form-text text-muted">AAAA-MM-DD</small>
+                </div>
+                
+            </div>
+            <div class="form-group row">
+                <label for="nacionalidadInput" class="col-2 col-form-label">Fecha Termino</label>
+                <div class="col-10">
+                    <input class="form-control" type="text" value="" id="nacionalidadInput" name="fecha_termino">
+                    <small id="medioHelp" class="form-text text-muted">AAAA-MM-DD</small>
+                </div>
+                
+            </div>
+            
+            <br/>
+            <button type="submit" class="btn btn-primary">Buscar</button>
+        </form>
   	<h1 class="my-4">Vehiculos 
     	<small>Selecciona el vehiculo que mas se acomode a tu viaje</small>
   	</h1>
