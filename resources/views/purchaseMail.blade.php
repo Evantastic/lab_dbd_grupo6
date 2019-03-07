622f9770ff682a8ce8d542591fb20c10a82d3d9d
Unknown <claudio.urrutia.b@usach.cl>
2019-03-07 00:33:02 -0300

    mail error

---
+++ resources/views/purchaseMail.blade.php
@@ -0,0 +1,21 @@
+ <div class="container">
+        <div class="align-content-center">
+
+            <br/>
+            <div class="jumbotron jumbotron-fluid">
+                <div class="container">
+                    <h1 class="display-4">Compra realizada</h1>
+                    <p class="lead">
+                        Id de Compra: {{$compra->id}}<br/>
+                        Compra asociada a: {{$user->email}}<br/>
+                        Recorrido: {{$recorrido->id}}
+                    </p>
+                </div>
+            </div>
+
+            <br/><br/>
+
+            <br/><br/>
+
+        </div>
+    </div>
